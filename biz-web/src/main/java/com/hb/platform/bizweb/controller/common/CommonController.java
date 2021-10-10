ff1b5725764cdb834e2c8fc87b29b7d32be67c8f
黄彪 <191059014@qq.com>
2021-10-10 17:11:11 +0800

    调整目录

--- biz-web/src/main/java/com/hb/platform/bizweb/controller/common/CommonController.java
+++ biz-web/src/main/java/com/hb/platform/bizweb/controller/common/CommonController.java
@@ -1,16 +1,23 @@
 package com.hb.platform.bizweb.controller.common;
 
+import com.hb.unic.base.annotation.InOutLog;
 import com.hb.unic.base.common.Result;
+import com.hb.unic.base.controller.BaseController;
 import com.hb.unic.common.standard.Dropdown;
+import com.hb.unic.common.util.ExcelUtils;
 import com.hb.unic.rbac.common.enums.ResourceType;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 常用的数据在这里
@@ -19,7 +26,24 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/common")
-public class CommonController {
+public class CommonController extends BaseController implements InitializingBean {
+
+    /**
+     * excel模板头部
+     */
+    private Map<String, List<String>> excelTemplateHeaders = new HashMap<>();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        initExcelTemplateHeaders();
+    }
+
+    /**
+     * 初始化excel模板头部
+     */
+    private void initExcelTemplateHeaders() {
+        excelTemplateHeaders.put("user", Arrays.asList("用户名", "手机号"));
+    }
 
     /**
      * 获取下拉框
@@ -28,6 +52,7 @@ public class CommonController {
      *            类型
      * @return 结果
      */
+    @InOutLog
     @GetMapping("/dropdown/{type}")
     public Result<List<Dropdown>> getDropdownByType(@PathVariable("type") String type) {
         List<Dropdown> list = new ArrayList<>();
@@ -40,6 +65,18 @@ public class CommonController {
                 break;
         }
         return Result.success(list);
+    }
+
+    /**
+     * 下载excel模板
+     * 
+     * @param type
+     *            模板类型
+     */
+    @InOutLog(printOutLog = false)
+    @PostMapping("/excel/template/{type}")
+    public void downloadTemplate(@PathVariable("type") String type) {
+        ExcelUtils.export(response, type + ExcelUtils.XLS, excelTemplateHeaders.get(type), null);
     }
 
 }
